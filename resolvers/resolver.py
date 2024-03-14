9b0e9cd954b9f3997b0ad36ebeda96f2365581a4
akshala <akshala.bhatnagar@gmail.com>
2024-03-13 19:40:33 -0700

    count resolver added

--- resolvers/resolver.py
+++ resolvers/resolver.py
@@ -1,41 +1,7 @@
 from config.es import es
 from config.settings import settings
-from models.annoq_model import AnnoqDataType
-from models.helper_models import AggregationItem, Bucket, DocCount, Field, PageArgs
-import re
-import requests
-from config.settings import settings
-
-def to_graphql_name(name):
-    if name[0].isdigit():
-        return f"x_{name}"
-    name = re.sub(r'\([^)]*\)', '', name)
-    name = re.sub(r'\/[^\/]*', '', name)
-    name = name.replace('-', '_')
-    name = name.replace('+', '')
-    return name
-
-
-def convert_hits(hits, aggregations):
-    compliant_results = []
-    for hit in hits:
-        source = hit['_source']
-        compliant_source = {to_graphql_name(key): value for key, value in source.items()}
-
-        data = {}
-        for key, val in compliant_source.items():
-           data[key] = Field(value=val, aggs=AggregationItem(doc_count=aggregations[key]['doc_count'] if key in aggregations else None,
-                              min=aggregations[f'{key}_min']['value'] if f'{key}_min' in aggregations else None,
-                              max=aggregations[f'{key}_max']['value'] if f'{key}_max' in aggregations else None,
-                              histogram=[Bucket(key=b['key'], doc_count=b['doc_count']) for b in aggregations['histogram']['buckets']] 
-                              if 'histogram' in aggregations else None,
-                              missing=DocCount(doc_count=aggregations[f'{key}_missing']['doc_count']) if f'{key}_missing' in aggregations else None,
-                              frequency=[Bucket(key=b['key'], doc_count=b['doc_count']) for b in aggregations[f'{key}_frequency']['buckets']]))
-           
-        data['id']  = hit['_id']
-            
-        compliant_results.append(AnnoqDataType(**data))
-    return compliant_results
+from models.helper_models import PageArgs
+from resolvers.resolver_helper import IDs_query, annotation_query, chromosome_query, convert_hits, gene_query, get_aggregation_query, rsID_query, rsIDs_query
 
 
 # Query for getting all annotations, no filter, size 20
@@ -43,12 +9,13 @@ async def get_annotations(es_fields: list[str]):
     resp = await es.search(
           index = settings.ES_INDEX,
           source = es_fields,
-          query = {"match_all": {}},
+          query = annotation_query(),
           aggs = await get_aggregation_query(es_fields),
           size = 20
     )
     results = convert_hits(resp['hits']['hits'], resp['aggregations'])  
     return results
+
 
 # Query for getting annotation by chromosome with start and end range of pos
 async def search_by_chromosome(es_fields: list[str], chr: str, start: int, end: int, page_args=PageArgs):
@@ -60,14 +27,7 @@ async def search_by_chromosome(es_fields: list[str], chr: str, start: int, end: 
           source = es_fields,
           from_= page_args.from_,
           size = page_args.size,
-          query = {
-              "bool": {
-                    "must": [
-                      {"term": {"chr": chr}},
-                      {"range": {"pos": {"gte": start, "lte": end}}}
-                    ]
-              }
-          },
+          query = chromosome_query(chr, start, end),
           aggs = await get_aggregation_query(es_fields)
     )
     results = convert_hits(resp['hits']['hits'], resp['aggregations']) 
@@ -83,13 +43,7 @@ async def search_by_rsID(es_fields: list[str], rsID:str, page_args=PageArgs):
           source = es_fields,
           from_= page_args.from_,
           size = page_args.size,
-          query = {
-              "bool": {
-                    "must": [
-                      {"term": {"rs_dbSNP151": rsID}},
-                    ]
-              }
-          },
+          query = rsID_query(rsID),
           aggs = await get_aggregation_query(es_fields)
     )
     results = convert_hits(resp['hits']['hits'], resp['aggregations'])    
@@ -105,17 +59,12 @@ async def search_by_rsIDs(es_fields: list[str], rsIDs: list[str], page_args=Page
           source = es_fields,
           from_= page_args.from_,
           size = page_args.size,
-          query = {
-              "bool": {
-                    "filter": [
-                      {"terms": {"rs_dbSNP151": rsIDs}}
-                    ]
-              }
-          },
+          query = rsIDs_query(rsIDs),
           aggs = await get_aggregation_query(es_fields)
     )
     results = convert_hits(resp['hits']['hits'], resp['aggregations'])    
     return results
+
 
 # query for VCF file
 async def search_by_IDs(es_fields: list[str], ids: list[str], page_args=PageArgs):
@@ -127,98 +76,28 @@ async def search_by_IDs(es_fields: list[str], ids: list[str], page_args=PageArgs
           source = es_fields,
           from_= page_args.from_,
           size = page_args.size,
-          query = {
-              "bool": {
-                 "filter": [
-                    {"ids": {"values": ids}}
-                 ]
-              }
-          },
+          query = IDs_query(ids),
           aggs = await get_aggregation_query(es_fields)
     )
     results = convert_hits(resp['hits']['hits'], resp['aggregations'])    
     return results
 
+
 # query for gene product
-async def search_by_gene(es_fields: list[str], gene:int, page_args=PageArgs):
+async def search_by_gene(es_fields: list[str], gene:str, page_args=PageArgs):
     if page_args is None:
       page_args = PageArgs
-      
-    response = requests.get(settings.ANNOTATION_API + '/gene?gene=' + str(gene))
 
-    if response.status_code == 200:
+      query = gene_query(gene)
 
-        data = response.json()
-        chr = data['gene_info']['contig']
-        start = data['gene_info']['start']
-        end = data['gene_info']['end']
-
+      if query is not None:
         resp = await es.search(
                 index = settings.ES_INDEX,
                 source = es_fields,
                 from_= page_args.from_,
                 size = page_args.size,
-                query = {
-                    "bool": {
-                        "must": [
-                          {"term": {"chr": chr}},
-                          {"range": {"pos": {"gte": start, "lte": end}}}
-                        ]
-                  }
-                },
+                query = query,
                 aggs = await get_aggregation_query(es_fields)
         )
         results = convert_hits(resp['hits']['hits'], resp['aggregations'])    
         return results
-
-    
-async def get_aggregation_query(es_fields: list[str]):
-    results = dict()
-    for field in es_fields:
-        
-        results[field] = {
-           "filter" : {
-            "exists": {
-              "field": field
-            }
-           }
-        }
-
-        results[f'{field}_min'] = {
-          "min": {
-            "field": "pos"
-          }
-       }
-
-        results[f'{field}_max'] = {
-            "max": {
-              "field": "pos"
-            }
-        }
-
-        results[f'{field}_frequency'] = {
-          "terms": {
-            "field": "pos",
-            "min_doc_count": 0,
-            "size": 20
-          }
-       }
-
-        results[f'{field}_missing'] = {
-          "missing": {
-              "field": "pos"
-            }
-       }
-
-        results['histogram'] = {
-          "histogram": {
-            "field": "pos",
-            "interval": 50000,
-            "extended_bounds": {
-              "min": 0,
-              "max": 500000
-            }
-          }
-       }
-
-    return results
--- schema/schema.py
+++ schema/schema.py
@@ -5,15 +5,20 @@ from models.annoq_model import AnnoqDataType
 from models.helper_models import PageArgs
 
 from resolvers.resolver import get_annotations, search_by_chromosome, search_by_gene, search_by_rsID, search_by_rsIDs, search_by_IDs
+from resolvers.resolver_count import count_by_IDs, count_by_chromosome, count_by_gene, count_by_rsID, count_by_rsIDs, get_annotations_count
 from utils import get_selected_fields
 
 @strawberry.type
 class Query:
     
     @strawberry.field
-    async def annotations(self, info: Info) -> List[AnnoqDataType]: 
+    async def GetAnnotations(self, info: Info) -> List[AnnoqDataType]: 
         fields = get_selected_fields(info)
         return await get_annotations(fields)
+    
+    @strawberry.field
+    async def CountAnnotations(self) -> int: 
+        return await get_annotations_count()
     
     @strawberry.field
     async def GetSNPsByChromosome(self, info: Info, chr: str, start: int, end: int, 
@@ -22,16 +27,28 @@ class Query:
         return await search_by_chromosome(fields, chr, start, end, page_args)
     
     @strawberry.field
+    async def CountSNPsByChromosome(self, chr: str, start: int, end: int) -> int:
+        return await count_by_chromosome(chr, start, end)
+    
+    @strawberry.field
     async def GetSNPsByRsID(self, info: Info, rsID: str,
                             page_args: Optional[PageArgs] = None) -> List[AnnoqDataType]:
         fields = get_selected_fields(info)
         return await search_by_rsID(fields, rsID, page_args)
     
     @strawberry.field
+    async def CountSNPsByRsID(self, rsID: str) -> int:
+        return await count_by_rsID(rsID)
+    
+    @strawberry.field
     async def GetSNPsByRsIDs(self, info: Info, rsIDs: list[str],
                              page_args: Optional[PageArgs] = None) -> List[AnnoqDataType]:
         fields = get_selected_fields(info)
         return await search_by_rsIDs(fields, rsIDs, page_args)
+    
+    @strawberry.field
+    async def CountSNPsByRsIDs(self, rsIDs: list[str]) -> int:
+        return await count_by_rsIDs(rsIDs)
      
     @strawberry.field
     async def GetSNPsByIDs(self, info: Info, ids: list[str],
@@ -40,9 +57,15 @@ class Query:
         return await search_by_IDs(fields, ids, page_args)
     
     @strawberry.field
+    async def CountSNPsByIDs(self, ids: list[str]) -> int:
+        return await count_by_IDs(ids)
+    
+    @strawberry.field
     async def GetSNPsByGeneProduct(self, info: Info, gene: int,
                                    page_args: Optional[PageArgs] = None) -> List[AnnoqDataType]:
         fields = get_selected_fields(info)
         return await search_by_gene(fields, gene, page_args)
-
-
+    
+    @strawberry.field
+    async def CountSNPsByGeneProduct(self, gene: int) -> int:
+        return await count_by_gene(gene)
