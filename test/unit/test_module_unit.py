fdf51cf7ad2c2cfc8d15664375550082555cd5c9
akshala <akshala.bhatnagar@gmail.com>
2024-03-21 18:00:11 -0700

    elasticsearch and graphql tests added, requirements.txt updated

--- test/unit/test_module_unit.py
+++ test/unit/test_module_unit.py
@@ -1,6 +1,7 @@
 from src.config.es import es
 from src.config.settings import settings
 import pytest
+import requests
 
 @pytest.mark.asyncio_cooperative
 async def test_elasticsearch():
@@ -12,3 +13,18 @@ async def test_elasticsearch():
 async def test_elasticsearch_mappings_count():
     mapping = await es.indices.get_mapping(index=settings.ES_INDEX)
     assert len(list(mapping[settings.ES_INDEX]['mappings']['properties'].keys())) == 533
+
+
+@pytest.mark.asyncio_cooperative
+async def test_elasticsearch_query():
+    resp = await es.search(
+          index=settings.ES_INDEX,
+          query={"match_all": {}},
+          size=20,
+    )
+    assert len(resp['hits']['hits']) == 20
+
+
+def test_strawberry():
+    response = requests.get(settings.API_URL + 'graphql')
+    assert response.status_code == 200
