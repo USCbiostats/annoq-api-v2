50325e0aac4e9c63e7db11a2ba305fcea1feb76c
Ayaan Kakkar <ayan.kakkar@gmail.com>
2025-10-28 20:02:32 -0700

    Enforce consistent pagination logic in SNP API Endpoints

--- src/graphql/resolvers/api_snp_helper_resolver.py
+++ src/graphql/resolvers/api_snp_helper_resolver.py
@@ -1,30 +1,31 @@
-from typing import List
 from src.graphql.models.return_info_model import OutputSnpInfo
-#from src.graphql.models.snp_model import Snp, SnpList
 from src.graphql.models.generated.snp import SnpModel
 from src.utils import clean_field_name
 from src.data_adapter.snp_attributes import get_version_info
 
+
 def output_error_msg(message: str):
     return OutputSnpInfo(success=False, message=message)
 
-def convert_scroll_hits(es_fields, hits):
-    """
-    Converts hits from elasticsearch to ScrollSnp object
 
-    Params: hits: hits from elasticsearch
-    
+def convert_hits_to_output(es_fields: list[str], hits: list):
+    """
+    Converts hits from elasticsearch to OutputSnpInfo object
+
+    Params: es_fields: List of fields requested
+            hits: hits from elasticsearch
+
     Returns: OutputSnpInfo object
     """
     compliant_results = []
     for hit in hits:
-        source = hit['_source']
-        values = {clean_field_name(key): value for key, value in source.items()} 
-        # values['id']  = hit['_id']
+        source = hit["_source"]
+        values = {clean_field_name(key): value for key, value in source.items()}
         compliant_results.append(SnpModel(**values))
-           
-    
-    return OutputSnpInfo(success = True, message = "OK", details = compliant_results, version = get_version_info(es_fields))
-        
 
-
+    return OutputSnpInfo(
+        success=True,
+        message="OK",
+        details=compliant_results,
+        version=get_version_info(es_fields),
+    )
--- src/graphql/resolvers/api_snp_resolver.py
+++ src/graphql/resolvers/api_snp_resolver.py
@@ -10,12 +10,37 @@ from src.graphql.resolvers.helper_resolver import (
 from src.data_access_object.keyword_search import keyword_query_for_fields_with_filters
 from src.graphql.resolvers.api_snp_helper_resolver import (
     output_error_msg,
-    convert_scroll_hits,
+    convert_hits_to_output,
 )
-from src.graphql.models.generated.snp import SnpModel
-from src.graphql.models.return_info_model import OutputSnpInfo
-from src.utils import clean_field_name
-from src.data_adapter.snp_attributes import get_version_info
+
+
+async def _execute_search(
+    es_fields: list[str],
+    query: dict,
+    page_args: PageArgs,
+    error_message: str,
+):
+    """
+    Generic search execution function to eliminate code duplication.
+
+    Params: es_fields: List of fields to be returned in elasticsearch query
+            query: Elasticsearch query object
+            page_args: PageArgs object for pagination
+            error_message: Custom error message for this operation
+
+    Returns: OutputSnpInfo with list of Snps
+    """
+    try:
+        resp = await es.search(
+            index=settings.ES_INDEX,
+            source=es_fields,  # type: ignore
+            from_=page_args.from_,
+            size=page_args.size,
+            query=query,
+        )
+        return convert_hits_to_output(es_fields, resp["hits"]["hits"])
+    except Exception:
+        return output_error_msg(error_message)
 
 
 async def search_by_chromosome(
@@ -33,34 +58,19 @@ async def search_by_chromosome(
             chr: Chromosome number
             start: Start position
             end: End position
-            query_type: Type of query to be executed
             page_args: PageArgs object for pagination
             filter_args: FilterArgs object for field exists filter
 
-
-    Returns: List of Snps
+    Returns: OutputSnpInfo with list of Snps
     """
-    if page_args is None:
-        page_args = PageArgs
-
-    try:
-        resp = await es.search(
-            index=settings.ES_INDEX,
-            source=es_fields,
-            from_=page_args.from_,
-            size=STANDARD_PAGE_SIZE,
-            query=chromosome_query(chr, start, end, filter_args),
-            scroll="2m",
-        )
-
-        return await query_retrieve_all_res(
-            es_fields, resp, page_args.size, STANDARD_PAGE_SIZE
-        )
-    # except ConnectionError as e:
-    #     return OutputSnpInfo(operationInfo= BaseOutput(success = False, message = e.message), details = None)
-    except Exception:
-        message = "Unable to retrieve information for search by chromosome"
-        return output_error_msg(message)
+    page_args = page_args or PageArgs()
+    query = chromosome_query(chr, start, end, filter_args)
+    return await _execute_search(
+        es_fields,
+        query,
+        page_args,
+        "Unable to retrieve information for search by chromosome",
+    )
 
 
 async def search_by_rsIDs(
@@ -77,30 +87,23 @@ async def search_by_rsIDs(
             page_args: PageArgs object for pagination
             filter_args: FilterArgs object for field exists filter
 
-    Returns: List of Snps
+    Returns: OutputSnpInfo with list of Snps
     """
-    if page_args is None:
-        page_args = PageArgs
-
-    try:
-        resp = await es.search(
-            index=settings.ES_INDEX,
-            source=es_fields,
-            from_=page_args.from_,
-            size=STANDARD_PAGE_SIZE,
-            query=rsIDs_query(rsIDs, filter_args),
-            scroll="2m",
-        )
-        return await query_retrieve_all_res(
-            es_fields, resp, page_args.size, STANDARD_PAGE_SIZE
-        )
-    except Exception:
-        message = "Unable to retrieve information for search by RSIDS"
-        return output_error_msg(message)
+    page_args = page_args or PageArgs()
+    query = rsIDs_query(rsIDs, filter_args)
+    return await _execute_search(
+        es_fields,
+        query,
+        page_args,
+        "Unable to retrieve information for search by RSIDS",
+    )
 
 
 async def search_by_IDs(
-    es_fields: list[str], ids: list[str], page_args=PageArgs, filter_args=FilterArgs
+    es_fields: list[str],
+    ids: list[str],
+    page_args: PageArgs,
+    filter_args: FilterArgs | None = None,
 ):
     """
     Query for getting annotation by IDs
@@ -110,33 +113,24 @@ async def search_by_IDs(
             page_args: PageArgs object for pagination
             filter_args: FilterArgs object for field exists filter
 
-
-    Returns: List of Snps
+    Returns: OutputSnpInfo with list of Snps
     """
-    if page_args is None:
-        page_args = PageArgs
-
-    try:
-        resp = await es.search(
-            index=settings.ES_INDEX,
-            source=es_fields,
-            from_=page_args.from_,
-            size=page_args.size,
-            query=IDs_query(ids, filter_args),
-        )
-
-        return await query_return(es_fields, resp)
-    except Exception:
-        message = "Unable to retrieve information for search by IDS"
-        return output_error_msg(message)
+    page_args = page_args or PageArgs()
+    query = IDs_query(ids, filter_args)
+    return await _execute_search(
+        es_fields,
+        query,
+        page_args,
+        "Unable to retrieve information for search by IDS",
+    )
 
 
 async def search_by_keyword(
     es_fields: list[str],
     keyword: str,
-    page_args=PageArgs,
-    keyword_fields: list[str] = None,
-    filter_fields: list[str] = None,
+    page_args: PageArgs,
+    keyword_fields: list[str] | None = None,
+    filter_fields: list[str] | None = None,
 ):
     """
     Query for getting annotation by keyword
@@ -144,29 +138,21 @@ async def search_by_keyword(
     Params: es_fields: List of fields to be returned in elasticsearch query
             keyword: Keyword to be searched
             page_args: PageArgs object for pagination
+            keyword_fields: Fields to search keyword in
+            filter_fields: Fields that must exist
 
-    Returns: List of Snps
+    Returns: OutputSnpInfo with list of Snps
     """
-    if page_args is None:
-        page_args = PageArgs
-    try:
-        resp = await es.search(
-            index=settings.ES_INDEX,
-            source=es_fields,
-            from_=page_args.from_,
-            size=STANDARD_PAGE_SIZE,
-            query=keyword_query_for_fields_with_filters(
-                keyword, keyword_fields, filter_fields
-            ),
-            scroll="2m",
-        )
-
-        return await query_retrieve_all_res(
-            es_fields, resp, page_args.size, STANDARD_PAGE_SIZE
-        )
-    except Exception:
-        message = "Unable to retrieve information for search by keyword"
-        return output_error_msg(message)
+    page_args = page_args or PageArgs()
+    query = keyword_query_for_fields_with_filters(
+        keyword, keyword_fields, filter_fields
+    )
+    return await _execute_search(
+        es_fields,
+        query,
+        page_args,
+        "Unable to retrieve information for search by keyword",
+    )
 
 
 async def search_by_gene_product(
@@ -183,87 +169,19 @@ async def search_by_gene_product(
             page_args: PageArgs object for pagination
             filter_args: FilterArgs object for field exists filter
 
-    Returns: List of Snps
+    Returns: OutputSnpInfo with list of Snps
     """
-    if page_args is None:
-        page_args = PageArgs
-
+    page_args = page_args or PageArgs()
     query = gene_query(gene, filter_args)
-    try:
-        if query is not None:
-            resp = await es.search(
-                index=settings.ES_INDEX,
-                source=es_fields,
-                from_=page_args.from_,
-                size=STANDARD_PAGE_SIZE,
-                query=query,
-                scroll="2m",
-            )
-        else:
-            message = "Unable to construct query for search by gene product operation"
-            return output_error_msg(message)
 
-        return await query_retrieve_all_res(
-            es_fields, resp, page_args.size, STANDARD_PAGE_SIZE
+    if query is None:
+        return output_error_msg(
+            "Unable to construct query for search by gene product operation"
         )
-    except Exception:
-        message = "Unable to retrieve information for search by gene product"
-        return output_error_msg(message)
 
-
-async def query_return(es_fields, resp):
-    """
-    Common return function for all queries
-
-    Params: es_fields: List of fields to be returned in elasticsearch query
-            resp: Response from elasticsearch query
-    """
-    results = convert_scroll_hits(es_fields, resp["hits"]["hits"])
-    return results
-
-
-async def query_retrieve_all_res(
-    es_fields: list[str], resp: dict, page_size: int, standard_page_size: int
-):
-    """
-    Common return function for all queries
-
-    Params: es_fields: List of fields to be returned in elasticsearch query
-            resp: Response from elasticsearch query
-    """
-    max_num_scroll_requests = page_size // standard_page_size
-    count = 0
-    scroll_id = resp["_scroll_id"]
-    compliant_results = []
-
-    while len(resp["hits"]["hits"]):
-        for hit in resp["hits"]["hits"]:
-            source = hit["_source"]
-            values = {clean_field_name(key): value for key, value in source.items()}
-            # values['id']  = hit['_id']
-            compliant_results.append(SnpModel(**values))
-
-        count += 1
-        if count > max_num_scroll_requests:
-            await es.clear_scroll(scroll_id=scroll_id)
-            return OutputSnpInfo(
-                success=True,
-                message="OK",
-                details=compliant_results,
-                version=get_version_info(es_fields),
-            )
-
-        resp = await es.scroll(scroll_id=scroll_id, scroll="2s")
-
-        if scroll_id != resp["_scroll_id"]:
-            print(f"Error retrieving information for scroll id {scroll_id}")
-            return output_error_msg("Error retrieving SNP information")
-
-    # count = 0, standard_page_size or no matches for query
-    await es.clear_scroll(scroll_id=scroll_id)
-    return OutputSnpInfo(
-        success=True,
-        message="OK",
-        details=compliant_results,
-        version=get_version_info(es_fields),
+    return await _execute_search(
+        es_fields,
+        query,
+        page_args,
+        "Unable to retrieve information for search by gene product",
     )
