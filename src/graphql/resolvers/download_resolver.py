998ada3083f5805c859c7c31993b3b617abfcc88
Ayaan Kakkar <ayan.kakkar@gmail.com>
2025-10-28 23:07:37 -0700

    Update graphql download api to use more performant streaming method

--- src/graphql/resolvers/download_resolver.py
+++ src/graphql/resolvers/download_resolver.py
@@ -1,46 +1,37 @@
 import uuid
-from ...config.es import es
+from typing import AsyncGenerator, Any
 from ...config.settings import settings
 
 
-async def download_annotations(fields: list[str], resp: dict):
+async def download_annotations_from_stream(
+    fields: list[str], stream: AsyncGenerator[Any, None]
+):
     """
-    Download annotations from elasticsearch
+    Download annotations from a streaming source
 
-    Params: fields: List of fields to be returned in elasticsearch query
-            resp: elasticsearch response object
+    Params: fields: List of fields to be returned
+            stream: Async generator yielding SNP records
 
     Returns: string for download filename
     """
     count = 0
-    filename = str(uuid.uuid4()) + '.txt'
-    f = open(settings.SITE_DOWNLOAD_DIR + '/' + filename, 'w')
-    f.write('\t'.join(fields) + '\n')
+    filename = str(uuid.uuid4()) + ".txt"
+    f = open(settings.SITE_DOWNLOAD_DIR + "/" + filename, "w")
+    f.write("\t".join(fields) + "\n")
 
-    old_scroll_id = resp['_scroll_id']
+    async for doc in stream:
+        count += 1
+        if count > settings.SIZE_DOWNLOAD_SIZE:
+            f.close()
+            return "/downloads/" + filename
 
-    while len(resp['hits']['hits']):
-        for doc in resp['hits']['hits']:
-            count += 1
-            if count > settings.SIZE_DOWNLOAD_SIZE: 
-                return "/downloads/" + filename
-            
-            li = []
-            for k in fields:
-                if k == "id":
-                    li.append(str(doc['_id']))
-                else:
-                    li.append(str(doc['_source'].get(k, '.')))
-            f.write('\t'.join(li) + "\n")
+        li = []
+        for k in fields:
+            if k == "id":
+                li.append(str(doc["_id"]))
+            else:
+                li.append(str(doc["_source"].get(k, ".")))
+        f.write("\t".join(li) + "\n")
 
-        resp = await es.scroll(
-            scroll_id = old_scroll_id,
-            scroll = '2s'
-        )
-
-        if old_scroll_id != resp['_scroll_id']:
-            f.write("download error on:", resp['_scroll_id'])
-
-        old_scroll_id = resp['_scroll_id']
-
+    f.close()
     return "/downloads/" + filename
